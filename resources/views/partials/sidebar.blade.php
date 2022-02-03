02bd2b343aacfa7cb83f965b52fb7c2a9133f7f0
SteveOwago <steveowago@gmail.com>
2022-02-03 12:51:30 +0300

    Rename fonts

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -50,49 +50,49 @@
                                 </a>
                                 <ul>
                                     <li>
-                                        <a href="elements-buttons-standard.html">
+                                        <a href="elements-buttons-standard.html" target="_blank">
                                             <i class="metismenu-icon"></i>
                                             Buttons
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="elements-dropdowns.html">
+                                        <a href="elements-dropdowns.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Dropdowns
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="elements-icons.html">
+                                        <a href="elements-icons.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Icons
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="elements-badges-labels.html">
+                                        <a href="elements-badges-labels.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Badges
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="elements-cards.html">
+                                        <a href="elements-cards.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Cards
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="elements-list-group.html">
+                                        <a href="elements-list-group.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>List Groups
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="elements-navigation.html">
+                                        <a href="elements-navigation.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Navigation Menus
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="elements-utilities.html">
+                                        <a href="elements-utilities.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Utilities
                                         </a>
@@ -107,67 +107,67 @@
                                 </a>
                                 <ul>
                                     <li>
-                                        <a href="components-tabs.html">
+                                        <a href="components-tabs.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Tabs
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="components-accordions.html">
+                                        <a href="components-accordions.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Accordions
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="components-notifications.html">
+                                        <a href="components-notifications.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Notifications
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="components-modals.html">
+                                        <a href="components-modals.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Modals
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="components-progress-bar.html">
+                                        <a href="components-progress-bar.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Progress Bar
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="components-tooltips-popovers.html">
+                                        <a href="components-tooltips-popovers.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Tooltips &amp; Popovers
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="components-carousel.html">
+                                        <a href="components-carousel.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Carousel
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="components-calendar.html">
+                                        <a href="components-calendar.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Calendar
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="components-pagination.html">
+                                        <a href="components-pagination.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Pagination
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="components-scrollable-elements.html">
+                                        <a href="components-scrollable-elements.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Scrollable
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="components-maps.html">
+                                        <a href="components-maps.html" target="_blank">
                                             <i class="metismenu-icon">
                                             </i>Maps
                                         </a>
@@ -175,51 +175,42 @@
                                 </ul>
                             </li>
                             <li>
-                                <a href="tables-regular.html">
+                                <a href="tables-regular.html" target="_blank">
                                     <i class="metismenu-icon pe-7s-display2"></i>
                                     Tables
                                 </a>
                             </li>
                             <li class="app-sidebar__heading">Widgets</li>
                             <li>
-                                <a href="dashboard-boxes.html">
+                                <a href="dashboard-boxes.html" target="_blank">
                                     <i class="metismenu-icon pe-7s-display2"></i>
                                     Dashboard Boxes
                                 </a>
                             </li>
                             <li class="app-sidebar__heading">Forms</li>
                             <li>
-                                <a href="forms-controls.html">
+                                <a href="forms-controls.html" target="_blank">
                                     <i class="metismenu-icon pe-7s-mouse">
                                     </i>Forms Controls
                                 </a>
                             </li>
                             <li>
-                                <a href="forms-layouts.html">
+                                <a href="forms-layouts.html" target="_blank">
                                     <i class="metismenu-icon pe-7s-eyedropper">
                                     </i>Forms Layouts
                                 </a>
                             </li>
                             <li>
-                                <a href="forms-validation.html">
+                                <a href="forms-validation.html" target="_blank">
                                     <i class="metismenu-icon pe-7s-pendrive">
                                     </i>Forms Validation
                                 </a>
                             </li>
                             <li class="app-sidebar__heading">Charts</li>
                             <li>
-                                <a href="charts-chartjs.html">
+                                <a href="charts-chartjs.html" target="_blank">
                                     <i class="metismenu-icon pe-7s-graph2">
                                     </i>ChartJS
-                                </a>
-                            </li>
-                            <li class="app-sidebar__heading">PRO Version</li>
-                            <li>
-                                <a href="https://dashboardpack.com/theme-details/architectui-dashboard-html-pro/"
-                                    target="_blank">
-                                    <i class="metismenu-icon pe-7s-graph2">
-                                    </i>
-                                    Upgrade to PRO
                                 </a>
                             </li>
                         </ul>
